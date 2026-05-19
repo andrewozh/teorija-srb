a20877eb6a748ecb334d293dab4288397673fee5
andrewozh <andrew.ozhegov@gmail.com>
2026-05-19 12:07:14 +0200

    footer double safe area fix

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -64,7 +64,7 @@
 		flex: 1;
 		overflow: hidden;
 		padding-top: env(safe-area-inset-top);
-		padding-bottom: env(safe-area-inset-bottom);
+		padding-bottom: 0;
 		padding-left: env(safe-area-inset-left);
 		padding-right: env(safe-area-inset-right);
 	}
